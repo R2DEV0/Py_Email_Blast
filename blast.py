f591f23c75ba328d31f2eb2aa9d0e6ca6e79fb1e
Kev Chancey <r2devo@gmail.com>
2020-07-10 10:28:21 -0700

    Update blast.py

--- blast.py
+++ blast.py
@@ -7,7 +7,7 @@ email_address = os.environ.get('KEVIN_EMAIL')
 email_password = os.environ.get('py_email_blast')
 
 # my contact list to be email blasted #
-contacts = ['r2devo@gmail.com', ]
+contacts = ['email@gmail.com', ]
 
 # run a for loop to email each contact directly #
 for contact in contacts:
@@ -15,7 +15,7 @@ for contact in contacts:
     msg['Subject'] = 'Kevin Chancey / Web Developer'
     msg['From'] = email_address
     msg['To'] = contact
-    msg.set_content('Hi - My name is Kevin and I am a full-stack developer. I was hoping this email reached the hiring manager or could be forwarded to them please?\n\nCurrently, I am a freelance programmer and in search of a long-term career and wanted to see if your company had any open entry level positions for a software engineer or web developer. If so, I would love to chat!\n\nI have attached my cover letter and resume.\n\n\nThanks,\nKevin Chancey\n(206) 519-4870\nR2Devo@Gmail.com\nhttp://kevinchancey.xyz/')
+    msg.set_content('Hi - My name is Kevin and I am a full-stack developer. I was hoping this email reached the hiring manager or could be forwarded to them please?\n\nCurrently, I am a freelance programmer and in search of a long-term career and wanted to see if your company had any open entry level positions for a software engineer or web developer. If so, I would love to chat!\n\nI have attached my cover letter and resume.\n\n\nThanks,\nKevin Chancey\n(000)000-0000\nEmail@Gmail.com\nhttp://kevinchancey.xyz/')
 
 # attach my resume & cover letter to the email #
     files = ['Kevin_Chancey_Letter.pdf','Kevin_Chancey_Resume.pdf']
